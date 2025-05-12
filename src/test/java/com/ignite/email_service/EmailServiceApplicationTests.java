3ee4e5242c99d25e714dcb1f53692ffbf2817cc4
limaarthur <artglima.dev@gmail.com>
2025-05-12 20:06:49 -0300

    first commit

---
+++ src/test/java/com/ignite/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignite.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
