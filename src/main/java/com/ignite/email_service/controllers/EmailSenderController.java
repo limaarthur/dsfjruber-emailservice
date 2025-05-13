273b7fcb9be91df6e2befc2eae894f8cc3db3e99
limaarthur <artglima.dev@gmail.com>
2025-05-13 11:37:28 -0300

    feat: created controller

---
+++ src/main/java/com/ignite/email_service/controllers/EmailSenderController.java
@@ -0,0 +1,35 @@
+package com.ignite.email_service.controllers;
+
+import com.ignite.email_service.application.EmailSenderService;
+import com.ignite.email_service.core.EmailRequest;
+import com.ignite.email_service.core.exceptions.EmailSendingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/emails")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return  ResponseEntity.ok("Email send successfully");
+
+        } catch (EmailSendingException exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending e-mail");
+        }
+    }
+}
